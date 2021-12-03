a6d481ee920274d440fbf100ab47ce24b2c3f5fe
Brian Gomes <briangomes@sapo.pt>
2021-12-03 21:34:38 +0000

    Adicionar arquivos de projeto.

---
+++ StocksUdemy/Produtos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace StocksUdemy
+{
+    class Produtos
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmStock () {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+            
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+         
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " 
+                 + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " 
+                 + Quantidade + " unidades, Total: $ " 
+                 + ValorTotalEmStock().ToString("F2", CultureInfo.InvariantCulture) ;
+        }
+    }
+}
---
+++ StocksUdemy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace StocksUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produtos novoproduto = new Produtos();
+
+             Console.WriteLine("Digite os dados do produto:");
+             Console.Write("Nome: ");
+             novoproduto.Nome = Console.ReadLine();
+
+             Console.Write("Preço: ");
+             novoproduto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+             Console.Write("Quantidade em Stock: ");
+             novoproduto.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+             Console.WriteLine();
+             Console.WriteLine("Dados do produto digitado: " + novoproduto);
+
+             Console.WriteLine();
+             Console.WriteLine("Digite a quantidade a adicionar ao stock:");
+             int qnt = int.Parse (Console.ReadLine());
+             novoproduto.AdicionarProdutos(qnt);
+
+             Console.WriteLine("Dados Atualizados: " + novoproduto);
+
+             Console.WriteLine();
+             Console.WriteLine("Digite a quantidade a remover ao stock:");
+             qnt = int.Parse(Console.ReadLine());
+             novoproduto.RemoverProdutos(qnt);
+
+             Console.WriteLine("Dados Atualizados: " + novoproduto); 
+
+     
+
+
+        }
+    }
+}
